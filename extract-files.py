e3008808335a6ea28b5cb72dececbd6095d6e9b2
Mashopy <eliasgheeraert@gmail.com>
2026-01-05 21:07:55 +0000

    mt6893-common: Let's go full prebuilt for C2 vendor stack
    That's the only sane way to make C2 not kill itself randomly.
    
    Change-Id: I46d9f4fe1bf85db11b89ecfb2e2866272e4a2719

--- extract-files.py
+++ extract-files.py
@@ -30,8 +30,7 @@ lib_fixups: lib_fixups_user_type = {
 
 
 blob_fixups: blob_fixups_user_type = {
-     ('vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b', 'vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service'): blob_fixup()
-        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so')
+    'vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so'),
 
     ('vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron', 'vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so'): blob_fixup()
@@ -61,6 +60,58 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.13-impl.so': blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so')
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so')
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so')
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v31.so')
+        .replace_needed('libcodec2_hidl@1.1.so', 'libcodec2_hidl@1.1-v31.so')
+        .replace_needed('libcodec2_hidl@1.2.so', 'libcodec2_hidl@1.2-v31.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v31.so'),
+
+    'vendor/lib64/libcodec2_hidl@1.0-v31.so': blob_fixup()
+        .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-v33.so')
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin-v31.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v31.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    'vendor/lib64/libcodec2_hidl@1.1-v31.so': blob_fixup()
+        .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-v33.so')
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v31.so')
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin-v31.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v31.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    'vendor/lib64/libcodec2_hidl@1.2-v31.so': blob_fixup()
+        .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-v33.so')
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v31.so')
+        .replace_needed('libcodec2_hidl@1.1.so', 'libcodec2_hidl@1.1-v31.so')
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin-v31.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v31.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    ('vendor/lib64/libcodec2_hidl_plugin-v31.so',
+     'vendor/lib64/libsfplugin_ccodec_utils-v31.so'): blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v31.so'),
+
+    ('vendor/lib64/libcodec2_mtk_c2store.so',
+     'vendor/lib64/libcodec2_mtk_vdec.so',
+     'vendor/lib64/libcodec2_mtk_venc.so',
+     'vendor/lib64/libcodec2_vpp_qt_plugin.so',
+     'vendor/lib64/libcodec2_vpp_rs_plugin.so'): blob_fixup()
+        .replace_needed('libcodec2_soft_common.so', 'libcodec2_soft_common-v31.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v31.so')
+        .replace_needed('libsfplugin_ccodec_utils.so', 'libsfplugin_ccodec_utils-v31.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    'vendor/lib64/libcodec2_soft_common-v31.so': blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v31.so')
+        .replace_needed('libsfplugin_ccodec_utils.so', 'libsfplugin_ccodec_utils-v31.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    'vendor/lib64/libcodec2_vndk-v31.so': blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so'),
+
 }  # fmt: skip
 
 module = ExtractUtilsModule(
