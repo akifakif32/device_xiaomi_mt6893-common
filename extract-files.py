ad557ac4d340e78a224f99ddb74b93a276da7e90
akifakif32 <mhmtkfdrn@gmail.com>
2026-09-11 16:01:49 +0300

    mt6893-common: Add and patch missing libraries

--- extract-files.py
+++ extract-files.py
@@ -107,6 +107,15 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so')
         .replace_needed('libui.so', 'libui-v34.so'),
 
+    ('vendor/lib64/libcodec2_soft_mtk_mp3dec.so',
+     'vendor/lib64/libcodec2_soft_mtk_alacdec.so',
+     'vendor/lib64/libcodec2_soft_mtk_apedec.so',
+     'vendor/lib64/libcodec2_soft_mtk_imaadpcmdec.so',
+     'vendor/lib64/libcodec2_soft_mtk_msadpcmdec.so'): blob_fixup()
+        .replace_needed('libcodec2_soft_common.so', 'libcodec2_soft_common-v31.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v31.so')
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
+
     'vendor/lib64/libcodec2_soft_common-v31.so': blob_fixup()
         .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v31.so')
         .replace_needed('libsfplugin_ccodec_utils.so', 'libsfplugin_ccodec_utils-v31.so')
