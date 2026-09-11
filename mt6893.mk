bcb15f9b45d7f9384ae219482720e0a03ee699d4
akifakif32 <mhmtkfdrn@gmail.com>
2026-09-11 15:50:21 +0300

    mt6893-common: Add missing path for AEE

--- mt6893.mk
+++ mt6893.mk
@@ -282,6 +282,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
+    hardware/mediatek/libaedv \
     hardware/xiaomi
 
 # Shipping API level
